1cfb64bbbba708e1d26ecb21bf6152aa639e6617
Admin <maayush235@gmail.com>
2024-07-08 15:53:47 +0530

    Radhey Radhey

--- lib/day1(Animator)/views/HomeScreen/home_screnn.dart
+++ lib/day1(Animator)/views/HomeScreen/home_screnn.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-
-
-
 // Draggable: Dragtarget
-// 
+//
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,28 +13,56 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Match Game'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        children: [
-          Draggable(child: Container(
-            height: 60,
-            width: 100,
-            decoration: BoxDecoration(
-              color: Colors.teal
-            ),
-          ), feedback: Container(
-            height: 60,
-            width: 100,
-            decoration: BoxDecoration(
-                color: Colors.redAccent
-            ),
-          ))
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('Match Game'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Draggable(
+                data: 'Aayush',
+                child: Container(
+                alignment: Alignment.center,
+                  height: 60,
+                  width: 100,
+                  decoration: BoxDecoration(color: Colors.teal),
+                  child: Text('Aayush'),
+                ),
+                feedback: Material(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: 100,
+                    decoration: BoxDecoration(color: Colors.redAccent),
+                    child: Text('Aayush'),
+                  ),
+                ),
+              ),
+              Spacer(),
+              DragTarget(
+                
+                onAcceptWithDetails: (details) {
+                  if(details.data == 'Aayush')
+                    {
+                      color = Colors.cyan;
+                    }
+                },
+                builder: (context, candidateData, rejectedData) {
+                  return Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: 100,
+                    decoration: BoxDecoration(color: color),
+                    child: Text('Coding'),
+                  );
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
+
+
+Color color = Colors.purpleAccent;
